860f49d8b95462046b52a45066eec3c673365f3b
danishsharma496 <danishsharma496@gmail.com>
2023-04-13 21:32:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import searchLogo from './search.svg';
 import './App.css';
+import { useState , useEffect } from 'react';
+import MovieCard from './MovieCard';
 
-function App() {
+
+// dd9693d6
+ const API_URL = 'https://www.omdbapi.com?apikey=dd9693d6';
+const App = () => {
+
+  const [searchTerm , setSearchTerm]=useState("");
+  const [movies , setMovies]=useState([]);
+
+  
+  useEffect(()=>{
+    searchMovie("spiderman");
+  }, []);
+
+  const searchMovie= (title)=>{
+    fetch(`${API_URL}&s=${title}`)
+    .then(resp => resp.json())
+    .then(data => {
+      setMovies(data.Search)
+    })
+    .catch(error => console.error(error));
+  
+  }
+  // react state can only be changed using its own setter function
+
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+    <h1>Movie Land</h1>
+    <div className='search'>
+      <input
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
+            placeholder="Search for movies "
+     /> 
+      <img 
+        src={searchLogo}
+        alt = "search"
+        onClick={()=>searchMovie(searchTerm)}
+      />
     </div>
+  n
+
+
+     
+
+     {movies?.length > 0 ? (
+      <div className='container'>
+        {movies.map((movie)=>(
+          <MovieCard movie={movie}/>
+        ))}
+      </div>):
+      (
+        <div className='empty'>
+          <h2>No movies found</h2>
+        </div>
+      )
+
+    } 
+    </div>
+
+
   );
 }
 
